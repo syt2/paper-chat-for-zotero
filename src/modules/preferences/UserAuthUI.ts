31864b13fb09c0f8d0f2e00ec2933f6e5335dd0c
ytshen <dreamcontinue.cd@gmail.com>
2026-04-20 13:21:49 +0800

    chore: update hint token count

--- src/modules/preferences/UserAuthUI.ts
+++ src/modules/preferences/UserAuthUI.ts
@@ -13,7 +13,7 @@ import { showMessage } from "./utils";
 
 type AuthManagerType = ReturnType<typeof getAuthManager>;
 
-const LOW_BALANCE_WARNING_THRESHOLD = 5000;
+const LOW_BALANCE_WARNING_THRESHOLD = 10000;
 const PREFS_REFRESH_MAX_ATTEMPTS = 12;
 const PREFS_REFRESH_RETRY_DELAY_MS = 120;
 const TOPUP_ATTENTION_DURATION_MS = 8000;
